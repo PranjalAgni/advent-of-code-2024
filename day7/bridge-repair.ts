8f2e8e411fa6cb59586435de58095efa539736d9
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-12-13 11:57:45 +0530

    :rocket: fun maths + recursion based problems

---
+++ day7/bridge-repair.ts
@@ -0,0 +1,39 @@
+import path from "path";
+import { convertInputToList, readInput } from "../utils/data";
+
+function solveRecursive(total: number, values: number[], current: number, pos: number): boolean {
+  const N = values.length;
+  if (total === current && pos === N) {
+    return true;
+  }
+
+  if (current > total) {
+    return false;
+  }
+
+  if (pos >= N) return false;
+  const isAdditionSolvable = solveRecursive(total, values, current + values[pos], pos + 1);
+  const isMultiplicationSolvable = solveRecursive(total, values, current * values[pos], pos + 1);
+  return isAdditionSolvable || isMultiplicationSolvable;
+}
+
+function solveEquation(inputData: string[]): number {
+  let answer = 0;
+  for (const line of inputData) {
+    const [total, values] = line.split(":");
+    const valuesList = values
+      .trim()
+      .split(" ")
+      .map((x) => parseInt(x));
+    if (solveRecursive(+total, valuesList, valuesList[0], 1)) {
+      answer += +total;
+    }
+  }
+
+  return answer;
+}
+
+const INPUT_PATH = path.join(__dirname, "input.txt");
+const inputData = convertInputToList(await readInput(INPUT_PATH));
+const answer = solveEquation(inputData);
+console.log("Answer is here", answer);
